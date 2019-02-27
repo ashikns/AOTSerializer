9aeb056375827b3d228c39bac12c4e4a79a362cb
Ashik Salim <ashikns@gmail.com>
2019-02-27 10:47:31 +0530

    add IFormatter, IResolver interfaces

---
+++ src/AOTSerializer.Common/IFormatter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AOTSerializer.Common
+{
+    public interface IFormatter
+    {
+        int SerializeNonGeneric(ref byte[] bytes, int offset, object value, IResolver resolver);
+        object DeserializeNonGeneric(byte[] bytes, int offset, Type type, out int readSize, IResolver resolver);
+    }
+
+    public interface IFormatter<T> : IFormatter
+    {
+        int Serialize(ref byte[] bytes, int offset, T value, IResolver resolver);
+        T Deserialize(byte[] bytes, int offset, out int readSize, IResolver resolver);
+    }
+
+    public abstract class FormatterBase<T> : IFormatter<T>
+    {
+        public int SerializeNonGeneric(ref byte[] bytes, int offset, object value, IResolver resolver)
+        {
+            if (!(value is T))
+            {
+                throw new Exception($"{nameof(value)} should be of type {typeof(T)}");
+            }
+            return Serialize(ref bytes, offset, (T)value, resolver);
+        }
+
+        public object DeserializeNonGeneric(byte[] bytes, int offset, Type type, out int readSize, IResolver resolver)
+        {
+            if (type != typeof(T))
+            {
+                throw new Exception($"{nameof(type)} should be of type {typeof(T)}");
+            }
+            return Deserialize(bytes, offset, out readSize, resolver);
+        }
+
+        public abstract int Serialize(ref byte[] bytes, int offset, T value, IResolver resolver);
+        public abstract T Deserialize(byte[] bytes, int offset, out int readSize, IResolver resolver);
+    }
+}
---
+++ src/AOTSerializer.Common/IResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace AOTSerializer.Common
+{
+    public interface IResolver
+    {
+        IFormatter GetFormatter(Type t);
+        IFormatter<T> GetFormatter<T>();
+    }
+
+    public abstract class CachedResolver : IResolver
+    {
+        protected ConcurrentDictionary<Type, IFormatter> FormatterCache { get; private set; }
+
+        protected CachedResolver()
+        {
+            FormatterCache = new ConcurrentDictionary<Type, IFormatter>();
+        }
+
+        public IFormatter<T> GetFormatter<T>()
+        {
+            return (IFormatter<T>)GetFormatter(typeof(T));
+        }
+
+        public IFormatter GetFormatter(Type t)
+        {
+            if (!FormatterCache.TryGetValue(t, out var formatter))
+            {
+                formatter = FindFormatter(t);
+                FormatterCache.TryAdd(t, formatter);
+            }
+
+            return formatter;
+        }
+
+        protected abstract IFormatter FindFormatter(Type t);
+    }
+}
