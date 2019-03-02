d53c7d7429e652c1fd3be72e0f07ba43986ebddb
Ashik Salim <ashikns@gmail.com>
2019-03-02 16:40:36 +0530

    add test project

---
+++ tests/AOTSerializer.Tests/CollectionTest.cs
@@ -0,0 +1,50 @@
+﻿using AOTSerializer.Common;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xunit;
+
+namespace AOTSerializer.Tests
+{
+    public class CollectionTest : IClassFixture<ResolverFixture>
+    {
+        private IResolver Resolver { get; }
+
+        public CollectionTest(ResolverFixture fixture)
+        {
+            Resolver = fixture.Resolver;
+        }
+
+        private T Convert<T>(T value)
+        {
+            return Serializer.Deserialize<T>(Serializer.Serialize(value, Resolver), Resolver);
+        }
+
+        public static object[][] collectionTestData = new object[][]
+        {
+            new object[]{ new int[]{ 1,10, 100 } , null },
+            new object[]{ new List<int>{ 1,10, 100 } , null },
+        };
+
+        [Theory]
+        [MemberData(nameof(collectionTestData))]
+        public void ConcreteCollectionTest<T>(T x, T y)
+        {
+            Convert(x).IsStructuralEqual(x);
+            Convert(y).IsStructuralEqual(y);
+        }
+
+        [Fact]
+        public void ArraySegmentTest()
+        {
+            var test = new ArraySegment<byte>(new byte[] { 1, 10, 100 });
+            Convert(test).Is(test);
+            ArraySegment<byte>? nullableTest = new ArraySegment<byte>(new byte[] { 1, 10, 100 });
+            Convert(nullableTest).Is(nullableTest);
+            nullableTest = null;
+            Convert(nullableTest).IsNull();
+        }
+    }
+}
---
+++ tests/AOTSerializer.Tests/ResolverFixture.cs
@@ -0,0 +1,16 @@
+﻿using AOTSerializer.Common;
+using AOTSerializer.Resolvers;
+
+namespace AOTSerializer.Tests
+{
+    public class ResolverFixture
+    {
+        public IResolver Resolver { get; }
+
+        public ResolverFixture()
+        {
+            Resolver = new CompositeResolver(new[] { Json.Resolvers.BuiltinResolver.Instance }, null);
+            //Resolver = new CompositeResolver(new[] { MessagePack.Resolvers.BuiltinResolver.Instance }, null);
+        }
+    }
+}
