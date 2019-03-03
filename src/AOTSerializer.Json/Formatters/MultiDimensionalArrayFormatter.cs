811dfa68cba4a7042d7d152a192e1a147610c8ef
Ashik Salim <ashikns@gmail.com>
2019-03-03 23:22:14 +0530

    add multi dimensional array formatter

---
+++ src/AOTSerializer.Json/Formatters/MultiDimensionalArrayFormatter.cs
@@ -0,0 +1,282 @@
+﻿using AOTSerializer.Common;
+using System.Collections.Generic;
+
+namespace AOTSerializer.Json.Formatters
+{
+    // multi dimensional array serialize to [[seq], [seq]]
+
+    public sealed class TwoDimensionalArrayFormatter<T> : FormatterBase<T[,]>
+    {
+        public override void Serialize(ref byte[] bytes, ref int offset, T[,] value, IResolver resolver)
+        {
+            if (value == null)
+            {
+                JsonUtility.WriteNull(ref bytes, ref offset);
+                return;
+            }
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var iLength = value.GetLength(0);
+            var jLength = value.GetLength(1);
+
+            JsonUtility.WriteBeginArray(ref bytes, ref offset);
+            for (int i = 0; i < iLength; i++)
+            {
+                if (i != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                JsonUtility.WriteBeginArray(ref bytes, ref offset);
+                for (int j = 0; j < jLength; j++)
+                {
+                    if (j != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                    formatter.Serialize(ref bytes, ref offset, value[i, j], resolver);
+                }
+                JsonUtility.WriteEndArray(ref bytes, ref offset);
+            }
+            JsonUtility.WriteEndArray(ref bytes, ref offset);
+        }
+
+        public override T[,] Deserialize(byte[] bytes, ref int offset, IResolver resolver)
+        {
+            if (JsonUtility.ReadIsNull(bytes, ref offset)) { return null; }
+
+            var buffer = new List<List<T>>(4);
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var guessInnerLength = 0;
+            var outerCount = 0;
+            JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+            while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+            {
+                if (outerCount++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+
+                var innerArray = new List<T>(guessInnerLength);
+                var innerCount = 0;
+                JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+                while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+                {
+                    if (innerCount++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+                    innerArray.Add(formatter.Deserialize(bytes, ref offset, resolver));
+                }
+
+                guessInnerLength = innerArray.Count;
+                buffer.Add(innerArray);
+            }
+
+            var t = new T[buffer.Count, guessInnerLength];
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                for (int j = 0; j < guessInnerLength; j++)
+                {
+                    t[i, j] = buffer[i][j];
+                }
+            }
+
+            return t;
+        }
+    }
+
+    public sealed class ThreeDimensionalArrayFormatter<T> : FormatterBase<T[,,]>
+    {
+        public override void Serialize(ref byte[] bytes, ref int offset, T[,,] value, IResolver resolver)
+        {
+            if (value == null)
+            {
+                JsonUtility.WriteNull(ref bytes, ref offset);
+                return;
+            }
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var iLength = value.GetLength(0);
+            var jLength = value.GetLength(1);
+            var kLength = value.GetLength(2);
+
+            JsonUtility.WriteBeginArray(ref bytes, ref offset);
+            for (int i = 0; i < iLength; i++)
+            {
+                if (i != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                JsonUtility.WriteBeginArray(ref bytes, ref offset);
+                for (int j = 0; j < jLength; j++)
+                {
+                    if (j != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                    JsonUtility.WriteBeginArray(ref bytes, ref offset);
+                    for (int k = 0; k < kLength; k++)
+                    {
+                        if (k != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                        formatter.Serialize(ref bytes, ref offset, value[i, j, k], resolver);
+                    }
+                    JsonUtility.WriteEndArray(ref bytes, ref offset);
+                }
+                JsonUtility.WriteEndArray(ref bytes, ref offset);
+            }
+            JsonUtility.WriteEndArray(ref bytes, ref offset);
+        }
+
+        public override T[,,] Deserialize(byte[] bytes, ref int offset, IResolver resolver)
+        {
+            if (JsonUtility.ReadIsNull(bytes, ref offset)) { return null; }
+
+            var buffer = new List<List<List<T>>>(4);
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var guessInnerLength2 = 0;
+            var guessInnerLength = 0;
+            var outerCount = 0;
+            JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+            while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+            {
+                if (outerCount++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+
+                var innerArray = new List<List<T>>(guessInnerLength);
+                var innerCount = 0;
+                JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+                while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+                {
+                    if (innerCount++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+
+                    var innerArray2 = new List<T>(guessInnerLength2);
+                    var innerCount2 = 0;
+                    JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+                    while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+                    {
+                        if (innerCount2++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+                        innerArray2.Add(formatter.Deserialize(bytes, ref offset, resolver));
+                    }
+
+                    guessInnerLength2 = innerArray2.Count;
+                    innerArray.Add(innerArray2);
+                }
+
+                guessInnerLength = innerArray.Count;
+                buffer.Add(innerArray);
+            }
+
+            var t = new T[buffer.Count, guessInnerLength, guessInnerLength2];
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                for (int j = 0; j < guessInnerLength; j++)
+                {
+                    for (int k = 0; k < guessInnerLength2; k++)
+                    {
+                        t[i, j, k] = buffer[i][j][k];
+                    }
+                }
+            }
+
+            return t;
+        }
+    }
+
+    public sealed class FourDimensionalArrayFormatter<T> : FormatterBase<T[,,,]>
+    {
+        public override void Serialize(ref byte[] bytes, ref int offset, T[,,,] value, IResolver resolver)
+        {
+            if (value == null)
+            {
+                JsonUtility.WriteNull(ref bytes, ref offset);
+                return;
+            }
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var iLength = value.GetLength(0);
+            var jLength = value.GetLength(1);
+            var kLength = value.GetLength(2);
+            var lLength = value.GetLength(3);
+
+            JsonUtility.WriteBeginArray(ref bytes, ref offset);
+            for (int i = 0; i < iLength; i++)
+            {
+                if (i != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                JsonUtility.WriteBeginArray(ref bytes, ref offset);
+                for (int j = 0; j < jLength; j++)
+                {
+                    if (j != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                    JsonUtility.WriteBeginArray(ref bytes, ref offset);
+                    for (int k = 0; k < kLength; k++)
+                    {
+                        if (k != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                        JsonUtility.WriteBeginArray(ref bytes, ref offset);
+                        for (int l = 0; l < lLength; l++)
+                        {
+                            if (l != 0) JsonUtility.WriteValueSeparator(ref bytes, ref offset);
+                            formatter.Serialize(ref bytes, ref offset, value[i, j, k, l], resolver);
+                        }
+                        JsonUtility.WriteEndArray(ref bytes, ref offset);
+                    }
+                    JsonUtility.WriteEndArray(ref bytes, ref offset);
+                }
+                JsonUtility.WriteEndArray(ref bytes, ref offset);
+            }
+            JsonUtility.WriteEndArray(ref bytes, ref offset);
+        }
+
+        public override T[,,,] Deserialize(byte[] bytes, ref int offset, IResolver resolver)
+        {
+            if (JsonUtility.ReadIsNull(bytes, ref offset)) { return null; }
+
+            var buffer = new List<List<List<List<T>>>>(4);
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var guessInnerLength3 = 0;
+            var guessInnerLength2 = 0;
+            var guessInnerLength = 0;
+            var outerCount = 0;
+            JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+            while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+            {
+                if (outerCount++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+
+                var innerArray = new List<List<List<T>>>(guessInnerLength);
+                var innerCount = 0;
+                JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+                while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+                {
+                    if (innerCount++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+
+                    var innerArray2 = new List<List<T>>(guessInnerLength2);
+                    var innerCount2 = 0;
+                    JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+                    while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+                    {
+                        if (innerCount2++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+
+                        var innerArray3 = new List<T>(guessInnerLength3);
+                        var innerCount3 = 0;
+                        JsonUtility.ReadIsBeginArrayWithVerify(bytes, ref offset);
+                        while (!JsonUtility.ReadIsEndArray(bytes, ref offset))
+                        {
+                            if (innerCount3++ != 0) { JsonUtility.ReadIsValueSeparatorWithVerify(bytes, ref offset); }
+                            innerArray3.Add(formatter.Deserialize(bytes, ref offset, resolver));
+                        }
+                        guessInnerLength3 = innerArray3.Count;
+                        innerArray2.Add(innerArray3);
+                    }
+
+                    guessInnerLength2 = innerArray2.Count;
+                    innerArray.Add(innerArray2);
+                }
+
+                guessInnerLength = innerArray.Count;
+                buffer.Add(innerArray);
+            }
+
+            var t = new T[buffer.Count, guessInnerLength, guessInnerLength2, guessInnerLength3];
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                for (int j = 0; j < guessInnerLength; j++)
+                {
+                    for (int k = 0; k < guessInnerLength2; k++)
+                    {
+                        for (int l = 0; l < guessInnerLength3; l++)
+                        {
+                            t[i, j, k, l] = buffer[i][j][k][l];
+                        }
+                    }
+                }
+            }
+
+            return t;
+        }
+    }
+}
---
+++ src/AOTSerializer.MessagePack/Formatters/MultiDimensionalArrayFormatter.cs
@@ -0,0 +1,242 @@
+﻿using AOTSerializer.Common;
+using System;
+
+namespace AOTSerializer.MessagePack.Formatters
+{
+    // multi dimensional array serialize to [i, j, [seq]]
+
+    public sealed class TwoDimensionalArrayFormatter<T> : FormatterBase<T[,]>
+    {
+        private const int ArrayLength = 3;
+
+        public override void Serialize(ref byte[] bytes, ref int offset, T[,] value, IResolver resolver)
+        {
+            if (value == null)
+            {
+                MessagePackBinary.WriteNil(ref bytes, ref offset);
+                return;
+            }
+
+            var i = value.GetLength(0);
+            var j = value.GetLength(1);
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            MessagePackBinary.WriteArrayHeader(ref bytes, ref offset, ArrayLength);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, i);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, j);
+
+            MessagePackBinary.WriteArrayHeader(ref bytes, ref offset, value.Length);
+            foreach (var item in value)
+            {
+                formatter.Serialize(ref bytes, ref offset, item, resolver);
+            }
+        }
+
+        public override T[,] Deserialize(byte[] bytes, ref int offset, IResolver resolver)
+        {
+            if (MessagePackBinary.IsNil(bytes, offset))
+            {
+                offset++;
+                return null;
+            }
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var len = MessagePackBinary.ReadArrayHeader(bytes, ref offset);
+            if (len != ArrayLength) throw new InvalidOperationException("Invalid T[,] format");
+
+            var iLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var jLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var maxLen = MessagePackBinary.ReadArrayHeader(bytes, ref offset);
+
+            var array = new T[iLength, jLength];
+
+            var i = 0;
+            var j = -1;
+            for (int loop = 0; loop < maxLen; loop++)
+            {
+                if (j < jLength - 1)
+                {
+                    j++;
+                }
+                else
+                {
+                    j = 0;
+                    i++;
+                }
+
+                array[i, j] = formatter.Deserialize(bytes, ref offset, resolver);
+            }
+
+            return array;
+        }
+    }
+
+    public sealed class ThreeDimensionalArrayFormatter<T> : FormatterBase<T[,,]>
+    {
+        private const int ArrayLength = 4;
+
+        public override void Serialize(ref byte[] bytes, ref int offset, T[,,] value, IResolver resolver)
+        {
+            if (value == null)
+            {
+                MessagePackBinary.WriteNil(ref bytes, ref offset);
+                return;
+            }
+
+            var i = value.GetLength(0);
+            var j = value.GetLength(1);
+            var k = value.GetLength(2);
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            MessagePackBinary.WriteArrayHeader(ref bytes, ref offset, ArrayLength);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, i);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, j);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, k);
+
+            MessagePackBinary.WriteArrayHeader(ref bytes, ref offset, value.Length);
+            foreach (var item in value)
+            {
+                formatter.Serialize(ref bytes, ref offset, item, resolver);
+            }
+        }
+
+        public override T[,,] Deserialize(byte[] bytes, ref int offset, IResolver resolver)
+        {
+            if (MessagePackBinary.IsNil(bytes, offset))
+            {
+                offset++;
+                return null;
+            }
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var len = MessagePackBinary.ReadArrayHeader(bytes, ref offset);
+            if (len != ArrayLength) throw new InvalidOperationException("Invalid T[,,] format");
+
+            var iLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var jLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var kLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var maxLen = MessagePackBinary.ReadArrayHeader(bytes, ref offset);
+
+            var array = new T[iLength, jLength, kLength];
+
+            var i = 0;
+            var j = 0;
+            var k = -1;
+            for (int loop = 0; loop < maxLen; loop++)
+            {
+                if (k < kLength - 1)
+                {
+                    k++;
+                }
+                else if (j < jLength - 1)
+                {
+                    k = 0;
+                    j++;
+                }
+                else
+                {
+                    k = 0;
+                    j = 0;
+                    i++;
+                }
+
+                array[i, j, k] = formatter.Deserialize(bytes, ref offset, resolver);
+            }
+
+            return array;
+        }
+    }
+
+    public sealed class FourDimensionalArrayFormatter<T> : FormatterBase<T[,,,]>
+    {
+        private const int ArrayLength = 5;
+
+        public override void Serialize(ref byte[] bytes, ref int offset, T[,,,] value, IResolver resolver)
+        {
+            if (value == null)
+            {
+                MessagePackBinary.WriteNil(ref bytes, ref offset);
+                return;
+            }
+
+            var i = value.GetLength(0);
+            var j = value.GetLength(1);
+            var k = value.GetLength(2);
+            var l = value.GetLength(3);
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            MessagePackBinary.WriteArrayHeader(ref bytes, ref offset, ArrayLength);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, i);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, j);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, k);
+            MessagePackBinary.WriteInt32(ref bytes, ref offset, l);
+
+            MessagePackBinary.WriteArrayHeader(ref bytes, ref offset, value.Length);
+            foreach (var item in value)
+            {
+                formatter.Serialize(ref bytes, ref offset, item, resolver);
+            }
+        }
+
+        public override T[,,,] Deserialize(byte[] bytes, ref int offset, IResolver resolver)
+        {
+            if (MessagePackBinary.IsNil(bytes, offset))
+            {
+                offset++;
+                return null;
+            }
+
+            var formatter = resolver.GetFormatterWithVerify<T>();
+
+            var len = MessagePackBinary.ReadArrayHeader(bytes, ref offset);
+            if (len != ArrayLength) throw new InvalidOperationException("Invalid T[,,,] format");
+
+            var iLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var jLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var kLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var lLength = MessagePackBinary.ReadInt32(bytes, ref offset);
+            var maxLen = MessagePackBinary.ReadArrayHeader(bytes, ref offset);
+
+            var array = new T[iLength, jLength, kLength, lLength];
+
+            var i = 0;
+            var j = 0;
+            var k = 0;
+            var l = -1;
+            for (int loop = 0; loop < maxLen; loop++)
+            {
+                if (l < lLength - 1)
+                {
+                    l++;
+                }
+                else if (k < kLength - 1)
+                {
+                    l = 0;
+                    k++;
+                }
+                else if (j < jLength - 1)
+                {
+                    l = 0;
+                    k = 0;
+                    j++;
+                }
+                else
+                {
+                    l = 0;
+                    k = 0;
+                    j = 0;
+                    i++;
+                }
+
+                array[i, j, k, l] = formatter.Deserialize(bytes, ref offset, resolver);
+            }
+
+            return array;
+        }
+    }
+}
