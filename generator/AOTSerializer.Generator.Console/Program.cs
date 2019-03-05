6cdbe8b799956a88489043ee68da133c81cfcd9a
Ashik Salim <ashikns@gmail.com>
2019-03-05 22:48:43 +0530

    add console project for generator

---
+++ generator/AOTSerializer.Generator.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace AOTSerializer.Generator.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var compilation = RoslynExtensions.GetCompilationFromFiles(
+                null,
+                null,
+                new[] {
+                    @"C:\Program Files\Unity\Hub\Editor\2018.3.7f1\Editor\Data\Managed" }
+                );
+
+            var additionalTypesToInclude = new Type[]
+            {
+                typeof(Vector2),
+                typeof(Vector3),
+                typeof(Vector4),
+                typeof(Quaternion),
+                typeof(Color),
+                typeof(Bounds),
+                typeof(Rect),
+
+                typeof(AnimationCurve),
+                typeof(RectOffset),
+                typeof(Gradient),
+                typeof(WrapMode),
+                typeof(GradientMode),
+                typeof(Keyframe),
+                typeof(Matrix4x4),
+                typeof(GradientColorKey),
+                typeof(GradientAlphaKey),
+                typeof(Color32),
+                typeof(LayerMask),
+            };
+
+            var codeGenerator = new Json.CodeGeneratorImpl(compilation, additionalTypesToInclude);
+            var generated = codeGenerator.Generate();
+
+            File.WriteAllText(@"C:\GitRepo\AOTSerializer\Generated.Json.cs", generated);
+
+            System.Console.WriteLine("Done!!");
+        }
+    }
+}
