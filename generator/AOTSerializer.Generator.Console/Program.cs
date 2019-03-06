35b44764e9aabc9668eae298ad0fe71354434a52
Ashik Salim <ashikns@gmail.com>
2019-03-06 21:20:07 +0530

    update console project

--- generator/AOTSerializer.Generator.Console/Program.cs
+++ generator/AOTSerializer.Generator.Console/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.IO;
-using UnityEngine;
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
 
 namespace AOTSerializer.Generator.Console
 {
@@ -8,40 +7,55 @@ namespace AOTSerializer.Generator.Console
     {
         private static void Main(string[] args)
         {
-            var compilation = RoslynExtensions.GetCompilationFromFiles(
-                null,
-                null,
-                new[] {
-                    @"C:\Program Files\Unity\Hub\Editor\2018.3.7f1\Editor\Data\Managed" }
+            //var additionalTypesToInclude = new Type[0];
+
+            //var compilation = RoslynExtensions.GetCompilation(
+            //    @"C:\Users\asalim\Desktop\msgraph-sdk-dotnet\Microsoft.Graph.sln",
+            //    null,
+            //    new string[] {
+            //        @"C:\Users\asalim\Desktop\msgraph-sdk-dotnet\src\Microsoft.Graph\Models\Generated",
+            //        //@"C:\Users\asalim\Desktop\msgraph-sdk-dotnet\src\Microsoft.Graph\Requests\Generated",
+            //        //@"C:\Users\asalim\Desktop\msgraph-sdk-dotnet\src\Microsoft.Graph.Core\Models",
+            //    },
+            //    (project, index, count) => { System.Console.WriteLine($"Compiled {project} - {index} of {count}"); }
+            //    );
+
+
+            var compilation2 = RoslynExtensions.GetCompilation(
+                @"C:\GitRepo\HoloBeam_Rearch\HoloBeam\HoloBeam.sln",
+                new string[] {
+                    @"C:\GitRepo\HoloBeam_Rearch\HoloBeam\Assets\_HoloBeam\DataTypes\AuthenticatedKey.cs",
+                    @"C:\GitRepo\HoloBeam_Rearch\HoloBeam\Assets\_HoloBeam\DataTypes\IdentityDescription.cs",
+                    @"C:\GitRepo\HoloBeam_Rearch\HoloBeam\Assets\_HoloBeam\DataTypes\ViewerStatus.cs"
+                },
+                new string[] {
+                    @"C:\GitRepo\HoloBeam_Rearch\HoloBeam\Assets\_HoloBeam\DataModels\_Contracts",
+                },
+                new string[] {
+                    @"C:\Program Files\Unity\Hub\Editor\2018.3.7f1\Editor\Data\Managed"
+                },
+                (project, index, count) => { System.Console.WriteLine($"Compiled {project} - {index} of {count}"); }
                 );
 
-            var additionalTypesToInclude = new Type[]
+            var additionalTypesToInclude2 = new[] { typeof(HoloBeam.DataModels.IceServer[]) };
+
+            var targetCompilation = compilation2.Result.TargetCompilation;
+            var referenceCompilations = compilation2.Result.ReferenceCompilations;
+            var additionalNamedTypes = new List<ITypeSymbol>();
+
+            foreach (var t in additionalTypesToInclude2)
             {
-                typeof(Vector2),
-                typeof(Vector3),
-                typeof(Vector4),
-                typeof(Quaternion),
-                typeof(Color),
-                typeof(Bounds),
-                typeof(Rect),
+                var namedType = RoslynExtensions.GetTypeSymbolForType(t, targetCompilation);
+                if (namedType != null)
+                {
+                    additionalNamedTypes.Add(namedType);
+                }
+            }
 
-                typeof(AnimationCurve),
-                typeof(RectOffset),
-                typeof(Gradient),
-                typeof(WrapMode),
-                typeof(GradientMode),
-                typeof(Keyframe),
-                typeof(Matrix4x4),
-                typeof(GradientColorKey),
-                typeof(GradientAlphaKey),
-                typeof(Color32),
-                typeof(LayerMask),
-            };
-
-            var codeGenerator = new Json.CodeGeneratorImpl(compilation, additionalTypesToInclude);
+            var codeGenerator = new Json.CodeGeneratorImpl(targetCompilation, additionalNamedTypes);
             var generated = codeGenerator.Generate();
 
-            File.WriteAllText(@"C:\GitRepo\AOTSerializer\Generated.Json.cs", generated);
+            System.IO.File.WriteAllText(@"C:\GitRepo\AOTSerializer\Generated.Json.cs", generated);
 
             System.Console.WriteLine("Done!!");
         }
